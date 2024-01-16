a24bb6cb5c570bea905ca01722a2136093464acf
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-01-16 13:08:29 +0530

    added static file access

--- harrybhaiNode/server.js
+++ harrybhaiNode/server.js
@@ -1,20 +1,30 @@
-// const http = require("http");
-import http from "http";
+// import http from "http";
 
-const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    res.write('"Hello from node js"');
-    res.end();
-  }
+// const server = http.createServer((req, res) => {
+//   if (req.url == "/") {
 
-  if (req.url == "/users") {
-    res.end("Users");
-  }
+//     res.write(index.html); // can not do this with http
+//     res.end();
+//   }
+
+//   if (req.url == "/users") {
+//     res.end("Users");
+//   }
+// });
+// ****************************************************************************
+import express from "express";
+let app = express();
+app.use(express.static("public"));
+
+app.get("/name", function (req, res) {
+  res.send("Welcome to home page");
 });
-(function (exports, module, require, __filename, __dirname) {
-  console.log("hi");
-})();
 
-server.listen(3000, (e) => {
+app.get("/:reqParams", (req, res) => {
+  console.log(req.query);
+  res.send(`hello ${req.params.reqParams}`);
+});
+
+app.listen(3000, (e) => {
   console.log("listening", e);
 });
