ed2628aa64add940a06306d11c24f2d9992b7f08
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-01-22 16:35:32 +0530

    Added playground

---
+++ harrybhaiNode/mongodb/playgroundone.mongodb.js
@@ -0,0 +1,60 @@
+use("Sigma");
+
+// let x = db.getCollection("practice").insertMany([
+//   {
+//     name: "John",
+//     age: 25,
+//     address: "New York",
+//   },
+//   {
+//     name: "Alice",
+//     age: 30,
+//     address: "Los Angeles",
+//   },
+//   {
+//     name: "Bob",
+//     age: 28,
+//     address: "Chicago",
+//   },
+//   {
+//     name: "Emma",
+//     age: 23,
+//     address: "San Francisco",
+//   },
+//   {
+//     name: "David",
+//     age: 27,
+//     address: "Miami",
+//   },
+//   {
+//     name: "Sophia",
+//     age: 26,
+//     address: "Seattle",
+//   },
+//   {
+//     name: "Ryan",
+//     age: 24,
+//     address: "Boston",
+//   },
+//   {
+//     name: "Olivia",
+//     age: 29,
+//     address: "Dallas",
+//   },
+//   {
+//     name: "Daniel",
+//     age: 31,
+//     address: "Houston",
+//   },
+//   {
+//     name: "Eva",
+//     age: 22,
+//     address: "Atlanta",
+//   },
+// ]);
+
+
+console.log(db.listCommands());
+
+let data = db.getUsers();
+console.log(data);
