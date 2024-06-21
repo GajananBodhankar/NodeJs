7a11521ade9a9a906f33a7aa21c33a470f99c919
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-06-21 18:14:06 +0530

    made a change

--- Cluster/app.js
+++ Cluster/app.js
@@ -1,6 +1,21 @@
 import cluster from 'cluster';
 import os from 'os'
+import express from "express";
+let totalCPU = os.cpus().length;
+if (cluster.isPrimary) {
+    for (let i = 0; i < totalCPU; i++) {
+        cluster.fork();
+    }
+} else {
+    let app = express();
 
-let totalCPU=os.cpus().length;
-
-console.log(totalCPU)
+    app.get("/", (req, res) => {
+        res.json({
+            message: `Hello my process id is ${process.pid}`
+        })
+    })
+    let PORT = process.env.PORT || 3000
+    app.listen(PORT, () => {
+        console.log("Listening to port", PORT)
+    })
+}
