4cbaf8e42aab4dc2d3af9bb97119ee8c56c565a2
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-06-28 21:59:09 +0530

    working on multithreading

---
+++ Multithreading/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+let PORT = process.env.PORT || 3000;
+
+let app = express();
+app.use(express.json())
+
+app.get('/non-blocking', (req, res) => {
+    res.json({
+        Message: "non blocking"
+    })
+})
+
+app.get('/blocking', (req, res) => {
+    let count = 0;
+    for (let i = 0; i < 30000000; i++) {
+        count++
+    }
+    res.json({
+        Message: `Count is ${count}`
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening to ${PORT}`)
+})
